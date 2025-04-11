8cf52384d5b875d8b16bd3242ed37b66e1a91ab1
Biswaranjan Mohanty <mbiswaranjan797@gmail.com>
2025-04-11 22:08:13 +0530

    Complete the Assignment

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
-import express from 'express';
-
+import express from "express";
+import bodyParser from "body-parser";
+import v1Router from "./routes/v1/v1Routes";
+import { config } from "./config/server.config";
 /**
  * DONOT DELETE THE BELOW app INSTANCE CREATION LINE i.e. const app = express();
  */
 const app = express();
+app.use(bodyParser.json());
 
+app.use("/api/v1", v1Router);
+
+app.use(`/api/${config.API_VERSION}`, v1Router);
+
+// Do not touch this line
+export { app };
 
 /**
  * DONOT DELETE THE BELOW EXPORT STATEMENT
