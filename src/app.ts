d5758e84741e48b831c077e31efb2cc59543381f
ddas4548 <ddas4548@gmail.com>
2025-04-12 21:27:19 +0530

    solving assignments

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
-
+import { json } from 'express';
+import v1Routes from './routes/v1/v1Routes'
+import { config } from './config/server.config';
 /**
  * DONOT DELETE THE BELOW app INSTANCE CREATION LINE i.e. const app = express();
  */
 const app = express();
+app.use(json())
+app.use(`/api/${config.API_VERSION}`, v1Routes);
 
 
 /**
